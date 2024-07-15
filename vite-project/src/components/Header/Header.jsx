27ab5399ef500a9102512b0f0414ed0a5e156bda
unknown <alexisbertell@gmail.com>
2024-07-15 12:23:41 +0200

    header creation

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Commandez votre plat préféré</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam officiis facere, ab voluptates quaerat, vitae assumenda odit veniam atque sunt nulla ea dolorem adipisci itaque! Quae nostrum commodi porro ab.
+        Molestias, temporibus? Repudiandae quasi ducimus atque iusto, ullam laudantium in, porro incidunt et minus repellendus asperiores, quod doloremque tempore quisquam corporis nemo. Vero repellendus, atque magni recusandae nam quo voluptatibus!
+        </p>
+       <button>Voir Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
