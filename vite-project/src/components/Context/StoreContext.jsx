d3e47d81566222bcdf79d5d48e5f4066562b3729
unknown <alexisbertell@gmail.com>
2024-07-17 23:31:05 +0200

    fix bug

--- vite-project/src/components/Context/StoreContext.jsx
+++ vite-project/src/components/Context/StoreContext.jsx
@@ -9,9 +9,9 @@ const [cartItems, setCartItems] = useState({});
 
 const addToCart = (itemId) => {
     if(!cartItems[itemId]) {
-        setCartItems((prev)=>({...prev,[itemId]:1}))
+        setCartItems(( prev )=>({...prev,[itemId]:1}))
     } else {
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        setCartItems(( prev )=>({...prev,[itemId]:prev[itemId]+1}))
     }
 }
 
