aaf2480296ce8c21b29c3cc55a0919f72eee426d
unknown <alexisbertell@gmail.com>
2024-07-19 18:56:47 +0200

    add PlaceOrder component and subtotal method

--- vite-project/src/pages/Cart/Cart.jsx
+++ vite-project/src/pages/Cart/Cart.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Cart.css';
-
+import { StoreContext } from '../../components/Context/StoreContext';
+import {useNavigate} from 'react-router-dom';
 
 const Cart = () => {
+
+const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
+
+const navigate = useNavigate();
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Produits</p>
+          <p>Titres</p>
+          <p>Prix</p>
+          <p>Quantité</p>
+          <p>Total</p>
+          <p>Supprimé</p>
+        </div>
+        <br />
+        <hr />
+
+        {food_list.map((item, index) => {
+          if(cartItems[item._id] > 0)
+          {
+            return (
+              <div>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}  €</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price*cartItems[item._id]}  €</p>
+                  <p className='cross' onClick={()=> removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
+              </div>
+
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sous-total</p>
+              <p>{getTotalCartAmount()} €</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Frais de livraison</p>
+              <p>{getTotalCartAmount() + 2} €</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+            <b>Total</b>
+            <b>{0}</b>
+            </div>
+            <button onClick={() => navigate('/order')} >PAYER</button>
+          </div>
+        </div>
+        <div className="cart-promo">
+            <div>
+              <p>Code Promo</p>
+              <div className='cart-promo-input'>
+                <input type="text" placeholder='Code Promo' />
+                <button>Soumettre</button>
+              </div>
+            </div>
+          </div>
+      </div>     
     </div>
   )
 }
--- vite-project/src/pages/PlaceOrder/PlaceOrder.jsx
+++ vite-project/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './PlaceOrder.css';
+import { StoreContext } from '../../components/Context/StoreContext';
 
 const PlaceOrder = () => {
+
+const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p>Informations de livraison</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='Nom' />
+          <input type="text" placeholder='Prénom' />
+        </div>
+        <input type="text" placeholder='Email' />
+        <input type="text" placeholder='Rue'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='Ville' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Code Postal' />
+          <input type="text" placeholder='Pays' />
+        </div>
+        <input type="text" placeholder='Téléphone' />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Sous-total</p>
+              <p>{getTotalCartAmount()} €</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Frais de livraison</p>
+              <p>{getTotalCartAmount() + 2} €</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+            <b>Total</b>
+            <b>{0}</b>
+            </div>
+            <button >Procéder au paiement</button>
+          </div>
+        </div>
+      </div>
+    </form>
   )
 }
 
